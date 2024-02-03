54dde890d282b44962c4e5917bb9416b7a646cad
Michael Wimble <mike@wimblerobotics.com>
2024-02-03 15:34:16 -0800

    Moved, deleted files. Minor fixups.

--- s_description/launch/description.launch.py
+++ s_description/launch/description.launch.py
@@ -3,13 +3,11 @@ import xacro
 import yaml
 
 import launch
+from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 import launch_ros.actions
-from launch.substitutions import LaunchConfiguration, Command, TextSubstitution
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
-
-
-from launch import LaunchDescription
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
