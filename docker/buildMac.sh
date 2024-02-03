d40ffe7155389c7e88c3a1e25e7676810443e998
Michael Wimble <mike@wimblerobotics.com>
2024-02-03 12:18:29 -0800

    Initial mac checkin

---
+++ docker/buildMac.sh
@@ -0,0 +1,1 @@
+docker build -t macros  -f DockerfileMac .
---
+++ docker/runMac.sh
@@ -0,0 +1,14 @@
+docker run \
+    -it \
+    -v /dev/:/dev/ \
+    -e "DISPLAY=$DISPLAY" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --device /dev/snd \
+    -e ALSADEV=hw:2,0 \
+    -v /home/ros/s_ws:/home/ros/s_ws \
+    --workdir /home/ros/s_ws \
+    --env="DISPLAY=host.docker.internal:0" 
+    --user 1000:1000 \
+    macros
+
+#docker run -it --device /dev/snd -e ALSADEV=hw:2,0 -v /dev/:/dev/ -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/ros/s_ws:/home/ros/s_ws --workdir /home/ros/s_ws  --user 1000:1000 macros
