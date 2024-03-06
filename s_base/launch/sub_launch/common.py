e5d50043967beb5689870b533c3b708acf6071cb
Michael Wimble <mike@wimblerobotics.com>
2024-03-06 15:23:23 -0800

    New default map for home

--- s_base/launch/sub_launch/common.py
+++ s_base/launch/sub_launch/common.py
@@ -17,7 +17,8 @@ ld = LaunchDescription()
 
 ld.add_action(DeclareLaunchArgument(
     name= 'map', 
-    default_value='home_map1.yaml',
+    # default_value='home_map1.yaml',
+    default_value='map_20240306.yaml',
     description='map file to use for navigation'))
 
 ld.add_action(DeclareLaunchArgument(
@@ -26,4 +27,5 @@ ld.add_action(DeclareLaunchArgument(
     description='Use simulation (Gazebo) clock if true'))
 
 map = LaunchConfiguration('map')
-map_path = os.path.join(s_base_directory_path, 'maps', 'home_map1.yaml')
+# map_path = os.path.join(s_base_directory_path, 'maps', 'home_map1.yaml')
+map_path = os.path.join(s_base_directory_path, 'maps', 'map_20240306.yaml')
